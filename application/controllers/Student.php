3bdb3bcaf74589f38a088732dbcc3801249405f6
booltek <51472134+booltek@users.noreply.github.com>
2020-07-09 00:39:44 +0800

    Update Student.php

--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -153,7 +153,8 @@ class Student extends Admin_Controller
         }
 
         $data['title']         = 'Student Details';
-        $student               = $this->student_model->get($id);
+        $student               = $this->student_model->get($id); 
+        var_dump($student);die;
         $gradeList             = $this->grade_model->get();
         $studentSession        = $this->student_model->getStudentSession($id);
         $timeline              = $this->timeline_model->getStudentTimeline($id, $status = '');
