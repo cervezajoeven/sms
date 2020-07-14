9f4f1fb5ff10cb4eeed3ca48a237ab7eb70bbfc0
Joeven <cervezajoeven@gmail.com>
2020-07-14 21:58:56 +0800

    LMS Update 4

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -661,8 +661,8 @@
                         <ul class="treeview-menu">
                             <li class="<?php echo set_Submenu('content/lesson'); ?>"><a href="<?php echo base_url(); ?>lms/lesson/index"><i class="fa fa-angle-double-right"></i> <?php echo $this->lang->line('lesson'); ?></a></li>
                             
-                            <li class="<?php echo set_Submenu('content/virtual'); ?>"><a href="<?php echo base_url(); ?>lms/lesson/virtual"><i class="fa fa-angle-double-right"></i> Google Meetup</a></li>
-                            <li class="<?php echo set_Submenu('content/virtual'); ?>"><a href="<?php echo base_url(); ?>admin/conference/timetable"><i class="fa fa-angle-double-right"></i> Zoom</a></li>
+                            <li class="<?php echo set_Submenu('content/virtual'); ?>"><a href="<?php echo base_url(); ?>lms/lesson/virtual"><i class="fa fa-angle-double-right"></i> Google Live Class</a></li>
+                            <li class="<?php echo set_Submenu('content/virtual'); ?>"><a href="<?php echo base_url(); ?>admin/conference/timetable"><i class="fa fa-angle-double-right"></i> Zoom Live Class</a></li>
                             <li class="<?php echo set_Submenu('content/shared_lesson'); ?>"><a href="<?php echo base_url(); ?>lms/lesson/shared"><i class="fa fa-angle-double-right"></i> Shared <?php echo $this->lang->line('lesson'); ?></a></li>
                             <li class="<?php echo set_Submenu('lms/assessment'); ?>"><a href="<?php echo base_url(); ?>lms/assessment/index"><i class="fa fa-angle-double-right"></i> <?php echo $this->lang->line('assessment'); ?></a></li>
                             <li class="<?php echo set_Submenu('content/lms_survey'); ?>"><a href="<?php echo base_url(); ?>lms/survey/index"><i class="fa fa-angle-double-right"></i> <?php echo $this->lang->line('lms_survey'); ?></a></li>
@@ -677,7 +677,7 @@
                             <li class="<?php echo set_Submenu('content/studymaterial'); ?>"><a href="<?php echo base_url(); ?>admin/content/studymaterial"><i class="fa fa-angle-double-right"></i> Module/Activities</a></li>
                             <li class="<?php echo set_Submenu('content/studymaterial'); ?>"><a href="<?php echo base_url(); ?>homework"><i class="fa fa-angle-double-right"></i> Homework/Projects</a></li>
                 
-                            <li class="<?php echo set_Submenu('content/syllabus'); ?>"><a href="<?php echo base_url(); ?>admin/content/syllabus"><i class="fa fa-angle-double-right"></i> Learning Plan </a></li>
+                            <!-- <li class="<?php echo set_Submenu('content/syllabus'); ?>"><a href="<?php echo base_url(); ?>admin/content/syllabus"><i class="fa fa-angle-double-right"></i> Learning Plan </a></li> -->
                             <!-- <li class="<?php echo set_Submenu('content/other'); ?>"><a href="<?php echo base_url(); ?>admin/content/other"><i class="fa fa-angle-double-right"></i> <?php echo $this->lang->line('other_downloads'); ?></a></li> -->
                         </ul>
                     </li>
