a88a76c88eed9effc9c9a895e68d41700bac28e4
booltek <51472134+booltek@users.noreply.github.com>
2020-08-19 17:51:30 +0800

    Update Onlinestudent_model.php

--- application/models/Onlinestudent_model.php
+++ application/models/Onlinestudent_model.php
@@ -134,7 +134,7 @@ class Onlinestudent_model extends MY_Model {
 
                 if ($sch_setting_detail->adm_auto_insert) {
                     if ($sch_setting_detail->adm_update_status) {
-                        $admission_no = $sch_setting_detail->adm_prefix . $sch_setting_detail->adm_start_from;
+                        // $admission_no = $sch_setting_detail->adm_prefix . $sch_setting_detail->adm_start_from;
                         //$last_student = $this->student_model->lastRecord();
                         $last_student = $this->student_model->lastRecordByAdmissionNo();
                         $last_admission_digit = str_replace($sch_setting_detail->adm_prefix, "", $last_student->admission_no);
@@ -190,7 +190,7 @@ class Onlinestudent_model extends MY_Model {
                     else if ($enroll_type == 'old_new') 
                     {
                         $student_id = $data['roll_no'] != null && $data['roll_no'] != '' ? $this->GetStudentID($data['roll_no']) : $this->GetStudentIDNumberByName($data['firstname'], $data['lastname']);
-                        $data['enrollment_type'] = $data['enrollment_type'] == 'old_new' ? 'old' : $data['enrollment_type'];
+                        $data['enrollment_type'] = 'old';
 
                         if (isset($student_id)) {
                             $this->db->where('id', $student_id);
