af1859cd204328a091474d0416e095991869cfe4
Joeven <cervezajoeven@gmail.com>
2020-08-20 00:01:29 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -118,7 +118,7 @@ if($HTTP_HOST=="localhost"){
 }elseif($HTTP_HOST=="stepsmandaluyong.com"){
   $config['enable_hooks'] = TRUE;
 }else{
-  $config['enable_hooks'] = FALSE;
+  $config['enable_hooks'] = TRUE;
 }
 
 /*
