93316f4670167c6113e2095ac6cc479bda4c5e28
booltek <51472134+booltek@users.noreply.github.com>
2020-08-21 23:03:34 +0800

    Update Homework.php

--- application/controllers/user/Homework.php
+++ application/controllers/user/Homework.php
@@ -18,32 +18,19 @@ class Homework extends Student_Controller
 
     public function index()
     {
-   
-
-
-
         $this->session->set_userdata('top_menu', 'Homework');
-
         $student_id            = $this->customlib->getStudentSessionUserID();
         $student_current_class = $this->customlib->getStudentCurrentClsSection();
-       
-  
         $data["created_by"]   = "";
         $data["evaluated_by"] = "";
-        $userdata             = $this->customlib->getLoggedInUserData();
-        
-
-
-
+        $userdata             = $this->customlib->getLoggedInUserData();    
         $result = $this->student_model->getRecentRecord($student_id);
-
         $class_id     = $student_current_class->class_id;
         $section_id   = $student_current_class->section_id;
         $homeworklist = $this->homework_model->getStudentHomeworkWithStatus($class_id, $section_id,$student_current_class->student_session_id);
-         $data["homeworklist"] = $homeworklist;
-       
-      
-
+        $data["homeworklist"] = $homeworklist;
+        $image_validate = $this->config->item('file_validate');
+        $data["max_file_size"] = number_format($image_validate['upload_size'] / 1048576, 2) . " MB";
         $this->load->view("layout/student/header");
         $this->load->view("user/homework/homeworklist", $data);
         $this->load->view("layout/student/footer");
@@ -51,7 +38,6 @@ class Homework extends Student_Controller
 
     public function upload_docs()
     {
-
         $homework_id         = $_REQUEST['homework_id'];
         $userdata            = $this->customlib->getLoggedInUserData();
         $student_id          = $userdata["student_id"];
@@ -59,37 +45,36 @@ class Homework extends Student_Controller
         $data['student_id']  = $student_id;
         $data['message']     = $_REQUEST['message'];
         // $data['id']=$_POST['assigment_id'];
-       $is_required=$this->homework_model->check_assignment($homework_id,$student_id);
-          $this->form_validation->set_rules('message', $this->lang->line('message'), 'trim|required|xss_clean');
-        
+        $is_required=$this->homework_model->check_assignment($homework_id, $student_id);
+        $this->form_validation->set_rules('message', $this->lang->line('message'), 'trim|required|xss_clean');        
         $this->form_validation->set_rules('file', $this->lang->line('attach_document'), 'trim|xss_clean|callback_handle_upload['.$is_required.']');
 
-        if ($this->form_validation->run() == FALSE) {
-          $msg=array(
-            'message'=>form_error('message'),
-            'file'=>form_error('file'),
-          );
-          $array = array('status' => 'fail', 'error' => $msg, 'message' => '');
-           
-        }else{
+        if ($this->form_validation->run() == FALSE) 
+        {
+            $msg=array(
+                'message'=>form_error('message'),
+                'file'=>form_error('file'),
+            );
+
+            $array = array('status' => 'fail', 'error' => $msg, 'message' => '');           
+        } 
+        else 
+        {
            // $insert_id           = $this->homework_model->upload_docs($data);
           if (isset($_FILES["file"]) && !empty($_FILES['file']['name'])) {
                 $time     = md5($_FILES["file"]['name'] . microtime());
                 $fileInfo = pathinfo($_FILES["file"]["name"]);
                 $img_name = $time . '.' . $fileInfo['extension'];           
-            $data['docs'] =  $img_name;
-            move_uploaded_file($_FILES["file"]["tmp_name"], "./uploads/homework/assignment/" . $data['docs']);
-           
-            $data['file_name']=$_FILES["file"]['name'];
-          
-            $this->homework_model->upload_docs($data);
+                $data['docs'] =  $img_name;
+                move_uploaded_file($_FILES["file"]["tmp_name"], "./uploads/homework/assignment/" . $data['docs']);            
+                $data['file_name']=$_FILES["file"]['name'];            
+                $this->homework_model->upload_docs($data);
+            }
+
+            $array = array('status' => 'success', 'error' => '', 'message' => $this->lang->line('success_message'));
         }
 
-         $array = array('status' => 'success', 'error' => '', 'message' => $this->lang->line('success_message'));
-        }
-
-        echo json_encode($array);
-        
+        echo json_encode($array);        
     }
 
     public function get_upload_docs($id){
@@ -103,12 +88,9 @@ class Homework extends Student_Controller
     {
         $data['homework_status']=$status;
         $data["title"] = "Homework Evaluation";
-
         $userdata = $this->customlib->getLoggedInUserData();
-
         $student_id           = $userdata["student_id"];
-        $result               = $this->homework_model->getRecord($id);
-      
+        $result               = $this->homework_model->getRecord($id);      
         $class_id             = $result["class_id"];
         $section_id           = $result["section_id"];
         $studentlist          = $this->homework_model->getStudents($class_id, $section_id);
@@ -119,18 +101,14 @@ class Homework extends Student_Controller
         //print_r($data["report"]);die;
         $data["created_by"]   = "";
         $data["evaluated_by"] = "";
-        $data["homeworkdocs"] = $this->homework_model->get_homeworkDocById($id);
+        $data["homeworkdocs"] = $this->homework_model->get_homeworkDocById($id);       
+        $create_data          = $this->staff_model->get($result["created_by"]);
 
-       
-            $create_data          = $this->staff_model->get($result["created_by"]);
-
-            $eval_data            = $this->staff_model->get($result["evaluated_by"]);
-            $created_by           = $create_data["name"];
-            $evaluated_by         = $eval_data["name"];
-            $data["created_by"]   = $created_by;
-            $data["evaluated_by"] = $evaluated_by;
-      
-
+        $eval_data            = $this->staff_model->get($result["evaluated_by"]);
+        $created_by           = $create_data["name"];
+        $evaluated_by         = $eval_data["name"];
+        $data["created_by"]   = $created_by;
+        $data["evaluated_by"] = $evaluated_by;
 
         $this->load->view("user/homework/homework_detail", $data);
     }
@@ -155,21 +133,18 @@ class Homework extends Student_Controller
         force_download($name, $data);
     }
 
-public function handle_upload($str,$is_required)
-    {
-     
+    public function handle_upload($str, $is_required)
+    {     
         $image_validate = $this->config->item('file_validate');
 
-        if (isset($_FILES["file"]) && !empty($_FILES['file']['name']) && $_FILES["file"]["size"] > 0) {
-
+        if (isset($_FILES["file"]) && !empty($_FILES['file']['name']) && $_FILES["file"]["size"] > 0) 
+        {
             $file_type         = $_FILES["file"]['type'];
             $file_size         = $_FILES["file"]["size"];
             $file_name         = $_FILES["file"]["name"];
             $allowed_extension = $image_validate['allowed_extension'];
             $ext               = pathinfo($file_name, PATHINFO_EXTENSION);
-
             $allowed_mime_type = $image_validate['allowed_mime_type'];
-
             $finfo = finfo_open(FILEINFO_MIME_TYPE);
             $mtype = finfo_file($finfo, $_FILES['file']['tmp_name']);
             finfo_close($finfo);
@@ -190,16 +165,19 @@ public function handle_upload($str,$is_required)
             }
 
             return true;
-        } else {
-          if($is_required==0){
-             $this->form_validation->set_message('handle_upload', 'Please choose a file to upload.');
-            return false;
-          }else{
-             return true;
-          }
-           
-        }
-       
+        } 
+        else 
+        {
+            if($is_required==0)
+            {
+                $this->form_validation->set_message('handle_upload', 'Please choose a file to upload.');
+                return false;
+            } 
+            else
+            {
+                return true;
+            }        
+        }       
 
     }
 
