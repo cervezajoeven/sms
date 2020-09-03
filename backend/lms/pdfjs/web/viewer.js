976015c36b476448e49805955c1e59f7ded68add
Joeven <cervezajoeven@gmail.com>
2020-09-03 22:22:27 +0800

    PDF Viewer Update

--- backend/lms/pdfjs/web/viewer.js
+++ backend/lms/pdfjs/web/viewer.js
@@ -1848,7 +1848,7 @@ var PDFViewerApplication = {
 };
 var validateFileURL = void 0;
 {
-  var HOSTED_VIEWER_ORIGINS = ['null', 'http://mozilla.github.io', 'https://mozilla.github.io'];
+  var HOSTED_VIEWER_ORIGINS = ['null', 'http://mozilla.github.io', 'https://mozilla.github.io' , 'https://media.campuscloudph.com'];
   validateFileURL = function validateFileURL(file) {
     if (file === undefined) {
       return;
