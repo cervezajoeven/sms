0ff9ec186ec826c45423f4e8ff191b5d29ad8ff5
Joeven <cervezajoeven@gmail.com>
2020-09-07 21:35:46 +0800

    Force DB Connection Close

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -19,8 +19,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // );
 
 // $hook['post_controller'] = array(     // 'post_controller' indicated execution of hooks after controller is finished
-//     'class' => 'Db_log',             // Name of Class
+//     'class' => 'Force_db_close',             // Name of Class
 //     'function' => 'logQueries',     // Name of function to be executed in from Class
 //     'filename' => 'db_log.php',    // Name of the Hook file
 //     'filepath' => 'hooks'         // Name of folder where Hook file is stored
 // );
+$hook['post_controller'] = array(     // 'post_controller' indicated execution of hooks after controller is finished
+    'class' => 'Force_db_close',             // Name of Class
+    'function' => 'close_db',     // Name of function to be executed in from Class
+    'filename' => 'Force_db_close.php',    // Name of the Hook file
+    'filepath' => 'hooks'         // Name of folder where Hook file is stored
+);
