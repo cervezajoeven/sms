cfff81051ed22f2011668018e892e000f37a641b
Joeven <cervezajoeven@gmail.com>
2020-09-08 15:17:10 +0800

    New LMS Update

--- lms_v2/application/config/database.php
+++ lms_v2/application/config/database.php
@@ -1,4 +1,5 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -23,7 +24,7 @@
 |				 cubrid, ibase, mssql, mysql, mysqli, oci8,
 |				 odbc, pdo, postgre, sqlite, sqlite3, sqlsrv
 |	['dbprefix'] You can add an optional prefix, which will be added
-				|				 to the table name when using the  Query Builder class
+|				 to the table name when using the  Query Builder class
 |	['pconnect'] TRUE/FALSE - Whether to use a persistent connection
 |	['db_debug'] TRUE/FALSE - Whether database errors should be displayed.
 |	['cache_on'] TRUE/FALSE - Enables/disables query caching
@@ -46,9 +47,9 @@
 |				'ssl_key'    - Path to the private key file
 |				'ssl_cert'   - Path to the public key certificate file
 |				'ssl_ca'     - Path to the certificate authority file
-|				'ssl_capath' - Path to a directory containing trusted CA certificats in PEM format
+|				'ssl_capath' - Path to a directory containing trusted CA certificates in PEM format
 |				'ssl_cipher' - List of *allowed* ciphers to be used for the encryption, separated by colons (':')
-|				'ssl_verify' - TRUE/FALSE; Whether verify the server certificate or not ('mysqli' only)
+|				'ssl_verify' - TRUE/FALSE; Whether verify the server certificate or not
 |
 |	['compress'] Whether or not to use client compression (MySQL only)
 |	['stricton'] TRUE/FALSE - forces 'Strict Mode' connections
@@ -65,104 +66,19 @@
 |
 | The $active_group variable lets you choose which connection group to
 | make active.  By default there is only one group (the 'default' group).
-				|
+|
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
-						*/
+*/
 $active_group = 'default';
 $query_builder = TRUE;
 
-
-$REQUEST_SCHEME = $_SERVER['REQUEST_SCHEME'];
-$HTTP_HOST = $_SERVER['HTTP_HOST'];
-$database_name = explode('.', $HTTP_HOST)[0];
-$godaddy = array("sim","beta");
-$campuscloudph = array("sms","rcamessjbcs","rcamessics","rcamesolgms","rcamesgcs","lipacitycolleges","lcc-silvercrest","htspreschool","htspg","htsmk","htslipa","dbti","c7preschool","lccsilvercrest");
-$local_db = "csl";
-// $local_db = "optimized_db";
-if($HTTP_HOST=="localhost"){
-
-  	$username = 'root';
-  	$password = '';
-  	$database = $local_db;
-  	$hostname = "localhost";
-  	$hostname_write = "localhost";
-
-}else if($HTTP_HOST=="www.stepsmandaluyong.com"){
-
-  	$username = 'admin';
-  	$password = 'J10o15e5V22n!4';
-  	$database = 'stepsmandaluyong';
-
-  	$hostname = "campusnovodbcluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-  	$hostname_write = "campusnovodbcluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-  	//$hostname = "campusnovodb-cluster.cluster-ro-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-  	//$hostname_write = "campusnovodb-cluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-
-}else if($HTTP_HOST=="stepsmandaluyong.com"){
-
-  	$username = 'admin';
-  	$password = 'J10o15e5V22n!4';
-  	$database = 'stepsmandaluyong';
-
-  	$hostname = "campusnovodbcluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-  	$hostname_write = "campusnovodbcluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-
-  	// $hostname = "campusnovodb-cluster.cluster-ro-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-  	// $hostname_write = "campusnovodb-cluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-
-}else if(in_array($database_name, $godaddy)){
-	
-	$username = 'joeven';
-  	$password = 'joeven241';
-  	$database = 'campus_'.$database_name;
-  	$hostname = "localhost";
-
-  	
-}else if(strpos($HTTP_HOST, "192.168.") !== false){
-	
-	$username = 'root';
-  	$password = '';
-  	$database = $local_db;
-  	$hostname = "localhost";
-
-
-}else if(in_array($database_name, $campuscloudph)){
-	
-	$username = 'admin';
-  	$password = 'J10o15e5V22n!4';
-  	$database = 'campus_'.$database_name;
-
-  	$hostname = "campusnovodbcluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-  	$hostname_write = "campusnovodbcluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-
-
-  	//read and write
-  	// $hostname = "campusnovodb-cluster.cluster-ro-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-  	// $hostname_write = "campusnovodb-cluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-
-}else{
-
-  	$username = 'admin';
-  	$password = 'J10o15e5V22n!4';
-  	$database = 'campus_'.$database_name;
-
-
-  	$hostname = "campusnovodbcluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-  	$hostname_write = "campusnovodbcluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-
-  	//read and write
-  	// $hostname = "campusnovodb-cluster.cluster-ro-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-  	// $hostname_write = "campusnovodb-cluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-}
-echo "model_load ";
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => $hostname,
-	// 'hostname' => '172.31.31.10',
-	'username' => $username,
-	'password' => $password,
-	'database' => $database,
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'csl',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -178,27 +94,3 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
-
-$db['write_db'] = array(
-	'dsn'	=> '',
-	'hostname' => $hostname_write,
-	// 'hostname' => '172.31.31.10',
-	'username' => $username,
-	'password' => $password,
-	'database' => $database,
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-// echo '<pre>';print_r($hostname);exit();
