94607d826225cfbb80791f6f89264fe0f928ba27
Joeven <cervezajoeven@gmail.com>
2020-09-14 02:22:18 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -116,6 +116,18 @@ if($HTTP_HOST=="localhost"){
   	// $hostname = "campusnovodb-cluster.cluster-ro-cuam6akcxov9.us-east-2.rds.amazonaws.com";
   	// $hostname_write = "campusnovodb-cluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
 
+}else if($database_name=="lcc-silvercrest"){
+
+  	$username = 'admin';
+  	$password = 'J10o15e5V22n!4';
+  	$database = 'campus_lcc-silvercrest';
+
+  	$hostname = "db-lcc-cluster.cluster-ro-cuam6akcxov9.us-east-2.rds.amazonaws.com";
+  	$hostname_write = "db-lcc-cluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
+
+  	// $hostname = "campusnovodb-cluster.cluster-ro-cuam6akcxov9.us-east-2.rds.amazonaws.com";
+  	// $hostname_write = "campusnovodb-cluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
+
 }else if(in_array($database_name, $godaddy)){
 	
 	$username = 'joeven';
