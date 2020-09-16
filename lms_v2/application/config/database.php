c3aaa7bce10411aaade2b14e36c5a0442f72af93
Joeven <cervezajoeven@gmail.com>
2020-09-17 02:54:52 +0800

    Update database.php

--- lms_v2/application/config/database.php
+++ lms_v2/application/config/database.php
@@ -174,8 +174,8 @@ if($HTTP_HOST=="localhost"){
     $password = 'J10o15e5V22n!4';
     $database = 'campus_phsi';
 
-    $hostname = "db-phsi-cluster.cluster-ro-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-  $hostname_write = "db-phsi-cluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
+    $hostname_read = "db-phsi-cluster.cluster-ro-cuam6akcxov9.us-east-2.rds.amazonaws.com";
+    $hostname = "db-phsi-cluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
 
 }else if($HTTP_HOST=="stepsmandaluyong.com"){
 
