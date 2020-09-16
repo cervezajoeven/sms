8474a0522480823deab4cbdbfde4ae5e012dc878
Joeven <cervezajoeven@gmail.com>
2020-09-16 21:14:15 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -132,8 +132,8 @@ if($HTTP_HOST=="localhost"){
   	$password = 'J10o15e5V22n!4';
   	$database = 'campus_baliuagu';
 
-  	$hostname = "csl-db-cluster.cluster-ro-cuam6akcxov9.us-east-2.rds.amazonaws.com";
-  	$hostname_write = "csl-db-cluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
+  	$hostname = "db-baliuagu-cluster.cluster-ro-cuam6akcxov9.us-east-2.rds.amazonaws.com";
+  	$hostname_write = "db-baliuagu-cluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
 
 }else if(in_array($database_name, $hts_array)){
     $username = 'admin';
