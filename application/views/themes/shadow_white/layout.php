2a61a927711a5a89212e2d9d24cf5bacc9da997b
booltek <51472134+booltek@users.noreply.github.com>
2020-09-18 10:52:21 +0800

    browser alert

--- application/views/themes/shadow_white/layout.php
+++ application/views/themes/shadow_white/layout.php
@@ -52,19 +52,21 @@
 
             // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
             function myFunction() {
-                //if (browser.name !== 'Chrome' && browser.name !== 'Firefox' && browser.name !== 'Opera') {
+                if (browser.name !== 'Chrome' && browser.name !== 'Firefox' && browser.name !== 'Opera') {
+                // if ($("#dialog").attr('style', 'display:block')) {
                     $("#dialog").dialog({		
                         position: {my: 'right bottom', at: 'right bottom', of: window}
                     });
-                //}
+                // }                    
+                }
             }
 
             $( function() {	
-                //if (browser.name !== 'Chrome' && browser.name !== 'Firefox' && browser.name !== 'Opera') {
+                if (browser.name !== 'Chrome' && browser.name !== 'Firefox' && browser.name !== 'Opera') {
                     $( "#dialog" ).dialog({		
                         position: {my: 'right bottom', at: 'right bottom', of: window}
                     });
-                //}
+                }
             });
         </script>
         <?php
