d95210913a86c84d51aa4c534e1fc9580f0de08d
Joeven <cervezajoeven@gmail.com>
2020-09-23 09:30:35 +0800

    Revert "Assessment Tracker and Simulator"
    This reverts commit e86dd3a4dba5e45a3a089accb5fe83597285b889.

--- application/views/lms/assessment/reports.php
+++ application/views/lms/assessment/reports.php
@@ -12,8 +12,118 @@
     <!-- Main content -->
     <section class="content">
         <div class="row">
+            <div class="col-md-7">
+                <!-- general form elements -->
+                <div class="box box-primary">
+                    <div class="box-header ptbnull">
+                        <h3 class="box-title titlefix">Students List</h3>
+                        <style type="text/css">
+                            .filter{
+                                width: 150px;
+                                display: inline;
+                            }
+                        </style>
+                        <div class="box-tools pull-right">
 
-            <div class="col-md-12">
+                            Filters:
+                            <select class="form-control filter section">
+                                <option value="all">All Section</option>
+                                <?php foreach ($sections as $section_key => $section_value) : ?>
+                                    <option value="<?php echo $section_value['id'] ?>"><?php echo $section_value['section'] ?></option>
+                                <?php endforeach; ?>
+                            </select>
+
+                            <select class="form-control filter gender">
+                                <option <?php echo ($gender=="all")?"selected":""; ?> value="all">All Gender</option>
+                                <option <?php echo ($gender=="male")?"selected":""; ?> value="male">Male</option>
+                                <option <?php echo ($gender=="female")?"selected":""; ?> value="female">Female</option>
+                            </select>
+                        </div><!-- /.box-tools -->
+                    </div><!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="mailbox-controls">
+                            <!-- Check all button -->
+                            <div class="pull-right">
+                                
+                            </div><!-- /.pull-right -->
+                        </div>
+                        <div class="mailbox-messages table-responsive">
+                            <div class="download_label"><?php echo $this->lang->line('content_list'); ?></div>
+                            <table class="table table-striped table-bordered table-hover example nowrap">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Gender</th>
+                                        <th>Grade</th>
+                                        <th>Section</th>
+                                        <th>Score</th>
+                                        <th>Percentage</th>
+                                        <th>Status</th>
+                                        <!-- <th>Essays</th> -->
+                                        <th class="text-right"><?php echo $this->lang->line('action'); ?>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($students as $list_key => $list_data): ?>
+
+                                        <tr>
+                                            <td class="mailbox-name">
+                                                <?php echo $list_data['lastname']?>, <?php echo $list_data['firstname']?>
+                                            </td>
+                                            <td class="mailbox-name">
+                                               <?php echo $list_data['gender']?>
+                                            </td>
+                                            <td class="mailbox-name">
+                                               <?php echo $list_data['class']?>
+                                            </td>
+                                            <td class="mailbox-name">
+                                                <?php echo $list_data['section']?>
+                                            </td>
+                                            <td>
+                                                <?php echo $list_data['score']?>
+                                            </td>
+                                            <td>
+                                                <?php echo round(($list_data['score']/$list_data['total_score'])*100) ?>%
+                                                <?php $current_percentage = round(($list_data['score']/$list_data['total_score'])*100) ?>
+                                            </td>
+                                            <td>
+                                                <?php echo ($current_percentage>=$assessment['percentage'])?"Pass":"Fail"; ?>
+                                            </td>
+                                            <!-- <td>
+                                                Checked
+                                            </td> -->
+                                            <td class="mailbox-date pull-right">
+                                                <?php if($role=="admin"): ?>
+
+
+                                                    <a data-placement="left" href="<?php echo site_url('lms/assessment/review/'.$list_data['assessment_id'].'/'.$list_data['student_id']);?>" class="btn btn-default btn-xs"  data-toggle="tooltip" title="View Answer Sheet" >
+                                                            <i class="fa fa-eye"></i>
+                                                    </a>
+
+                                                <?php elseif($role=="student"): ?>
+                                                    <a data-placement="left" href="<?php echo site_url('lms/assessment/review/'.$list_data['id']);?>" class="btn btn-default btn-xs"  data-toggle="tooltip" title="View Answer" >
+                                                            <i class="fa fa-eye"></i>
+                                                    </a>
+                                                    <a data-placement="left" href="<?php echo site_url('lms/assessment/answer/'.$list_data['id']);?>" class="btn btn-default btn-xs"  data-toggle="tooltip" title="Answer" >
+                                                            <i class="fa fa-edit"></i>
+                                                    </a>
+                                                <?php endif; ?>
+                                                
+                                            </td>
+                                        </tr>
+                                        <?php endforeach; ?>
+
+                                </tbody>
+                            </table><!-- /.table -->
+                        </div><!-- /.mail-box-messages -->
+
+                    </div><!-- /.box-body -->
+
+                </div>
+            </div><!--/.col (left) -->
+            
+            <div class="col-md-5">
                 <!-- general form elements -->
                 <div class="box box-primary">
                     <div class="box-header ptbnull">
@@ -86,127 +196,6 @@
 
                 </div>
             </div><!--/.col (left) -->
-
-
-            <div class="col-md-12">
-                <!-- general form elements -->
-                <div class="box box-primary">
-                    <div class="box-header ptbnull">
-                        <h3 class="box-title titlefix">Students List</h3>
-                        <style type="text/css">
-                            .filter{
-                                width: 150px;
-                                display: inline;
-                            }
-                        </style>
-                        <div class="box-tools pull-right">
-
-                            Filters:
-                            <select class="form-control filter section">
-                                <option value="all">All Section</option>
-                                <?php foreach ($sections as $section_key => $section_value) : ?>
-                                    <option value="<?php echo $section_value['id'] ?>"><?php echo $section_value['section'] ?></option>
-                                <?php endforeach; ?>
-                            </select>
-
-                            <select class="form-control filter gender">
-                                <option <?php echo ($gender=="all")?"selected":""; ?> value="all">All Gender</option>
-                                <option <?php echo ($gender=="male")?"selected":""; ?> value="male">Male</option>
-                                <option <?php echo ($gender=="female")?"selected":""; ?> value="female">Female</option>
-                            </select>
-                        </div><!-- /.box-tools -->
-                    </div><!-- /.box-header -->
-                    <div class="box-body">
-                        <div class="mailbox-controls">
-                            <!-- Check all button -->
-                            <div class="pull-right">
-                                
-                            </div><!-- /.pull-right -->
-                        </div>
-                        <div class="mailbox-messages table-responsive">
-                            <div class="download_label"><?php echo $this->lang->line('content_list'); ?></div>
-                            <table class="table table-striped table-bordered table-hover example nowrap">
-                                <thead>
-                                    <tr>
-                                        <th class="text-right"><?php echo $this->lang->line('action'); ?></th>
-                                        <th>Name</th>
-                                        <th>Gender</th>
-                                        <th>Grade</th>
-                                        <th>Section</th>
-                                        <th>Score</th>
-                                        <th>Percentage</th>
-                                        <th>Status</th>
-                                        <th>Browser</th>
-                                        <th>Device</th>
-                                        <th>Version</th>
-                                        <th>OS</th>
-                                        <!-- <th>Essays</th> -->
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php foreach ($students as $list_key => $list_data): ?>
-
-                                        <tr>
-                                            <td class="mailbox-date pull-right">
-                                                <center>
-                                                    <a data-placement="right" href="<?php echo site_url('lms/assessment/review/'.$list_data['assessment_id'].'/'.$list_data['student_id']);?>" class="btn btn-default btn-xs"  data-toggle="tooltip" title="View Answer Sheet" >
-                                                        <i class="fa fa-eye"></i>
-                                                    </a>
-                                                    <a data-placement="right" href="<?php echo site_url('lms_v2/index.php/lms/assessment/initialize/'.$list_data['student_id'].'/student/'.$list_data['assessment_id']);?>" class="btn btn-default btn-xs"  data-toggle="tooltip" title="Check Answer Sheet" >
-                                                        <i class="fa fa-play"></i>
-                                                    </a>
-                                                </center>
-                                                
-
-                                               
-                                            </td>
-                                            <td class="mailbox-name">
-                                                <?php echo $list_data['lastname']?>, <?php echo $list_data['firstname']?>
-                                            </td>
-                                            <td class="mailbox-name">
-                                               <?php echo $list_data['gender']?>
-                                            </td>
-                                            <td class="mailbox-name">
-                                               <?php echo $list_data['class']?>
-                                            </td>
-                                            <td class="mailbox-name">
-                                                <?php echo $list_data['section']?>
-                                            </td>
-                                            <td>
-                                                <?php echo $list_data['score']?>
-                                            </td>
-                                            <td>
-                                                <?php echo round(($list_data['score']/$list_data['total_score'])*100) ?>%
-                                                <?php $current_percentage = round(($list_data['score']/$list_data['total_score'])*100) ?>
-                                            </td>
-                                            <td>
-                                                <?php echo ($current_percentage>=$assessment['percentage'])?"Pass":"Fail"; ?>
-                                            </td>
-                                            <td>
-                                                <?php echo $list_data['browser']?>
-                                            </td>
-                                            <td>
-                                                <?php echo $list_data['device']?>
-                                            </td>
-                                            <td>
-                                                <?php echo $list_data['browser_version']?>
-                                            </td>
-                                            <td>
-                                                <?php echo $list_data['os_platform']?>
-                                            </td>
-                                        </tr>
-                                        <?php endforeach; ?>
-
-                                </tbody>
-                            </table><!-- /.table -->
-                        </div><!-- /.mail-box-messages -->
-
-                    </div><!-- /.box-body -->
-
-                </div>
-            </div><!--/.col (left) -->
-            
-            
 
 
             <!-- right column -->
