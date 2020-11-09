6beba4fdcea40d5353949f83af9eb4c12243994f
booltek <51472134+booltek@users.noreply.github.com>
2020-11-09 21:28:19 +0800

    Update ClassRecord_model.php

--- application/models/ClassRecord_model.php
+++ application/models/ClassRecord_model.php
@@ -9,12 +9,7 @@ class Classrecord_model extends MY_Model
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('setting_model');
-        $this->load->model('customfield_model');
-        $this->load->model('teacher_model');
-        $this->current_session = $this->setting_model->getCurrentSession();
-        $this->current_date    = $this->setting_model->getDateYmd();
-        $this->schoolname = $this->setting_model->getCurrentSchoolName(); 
+    
         //-- Load database for writing
         $this->writedb = $this->load->database('write_db', TRUE);
     }
