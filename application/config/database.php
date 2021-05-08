4c9edac6ca48803371ce2955e6cd7e4e17dbc06c
Erwin <erwin.nora@gmail.com>
2021-05-08 09:12:15 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -182,6 +182,13 @@ if ($HTTP_HOST == "localhost") {
 
 	$hostname = "campusnovodbcluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
 	$hostname_write = "campusnovodbcluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
+} else if ($HTTP_HOST == "sunnyvalechristian.com") {
+	$username = 'admin';
+	$password = 'J10o15e5V22n!4';
+	$database = 'campus_sunnyvale';
+
+	$hostname = "campusnovodbcluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
+	$hostname_write = "campusnovodbcluster.cluster-cuam6akcxov9.us-east-2.rds.amazonaws.com";
 } else {
 
 	$username = 'admin';
