20d21d8936819cba192828ddfb904fe25ec18d57
Erwin <erwin.nora@gmail.com>
2021-05-10 06:48:46 +0800

    Update newOnlineEnrollment.php

--- application/views/admin/onlinestudent/newOnlineEnrollment.php
+++ application/views/admin/onlinestudent/newOnlineEnrollment.php
@@ -14,7 +14,15 @@
                     </div><!-- /.box-header -->
                     <div class="box-body table-responsive">
                         <div class="mailbox-messages">
-                            <p>You will redirected to a new page for the new version of online enrollment.</p><a href="http://lms.campuscloudph.com/staff/login/<?php echo $id . "/" . $access_key ?>" target="_blank">Please click here to continue</a>
+                            <?php
+                            $host = $_SERVER['HTTP_HOST'];
+                            // $enrollmentUrl = "https://admission." . $host . ".s3-website.us-east-2.amazonaws.com/staff/login/";
+                            $enrollmentUrl = "https://admission." . $host . "/staff/login/";
+                            //"http://admission.aphsrizal.com.s3-website.us-east-2.amazonaws.com"; 
+                            ?>
+                            <!-- <p>You will redirected to a new page for the new version of online enrollment.</p><a href="http://lms.campuscloudph.com/staff/login/<?php //echo $id . "/" . $access_key 
+                                                                                                                                                                        ?>" target="_blank">Please click here to continue</a> -->
+                            <p>You will redirected to a new page for the new version of online enrollment.</p><a href="<?php echo $enrollmentUrl . $id . "/" . $access_key ?>" target="_blank">Please click here to continue</a>
                         </div><!-- /.mail-box-messages -->
                     </div><!-- /.box-body -->
                 </div>
