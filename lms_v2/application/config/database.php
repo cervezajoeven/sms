eaaacf930c76f08d85f6de6a383e20da35dc8c2f
Erwin <erwin.nora@gmail.com>
2021-07-08 17:58:30 +0800

    Update database.php

--- lms_v2/application/config/database.php
+++ lms_v2/application/config/database.php
@@ -247,24 +247,24 @@ $db['default'] = array(
    'failover' => array(),
    'save_queries' => TRUE
 );
-$db['read'] = array(
-   'dsn'   => '',
-   'hostname' => $hostname_read,
-   'username' => $username,
-   'password' => $password,
-   'database' => $database,
-   'dbdriver' => 'mysqli',
-   'dbprefix' => '',
-   'pconnect' => FALSE,
-   'db_debug' => (ENVIRONMENT !== 'production'),
-   'cache_on' => FALSE,
-   'cachedir' => '',
-   'char_set' => 'utf8',
-   'dbcollat' => 'utf8_general_ci',
-   'swap_pre' => '',
-   'encrypt' => FALSE,
-   'compress' => FALSE,
-   'stricton' => FALSE,
-   'failover' => array(),
-   'save_queries' => TRUE
-);
+// $db['read'] = array(
+//    'dsn'   => '',
+//    'hostname' => $hostname_read,
+//    'username' => $username,
+//    'password' => $password,
+//    'database' => $database,
+//    'dbdriver' => 'mysqli',
+//    'dbprefix' => '',
+//    'pconnect' => FALSE,
+//    'db_debug' => (ENVIRONMENT !== 'production'),
+//    'cache_on' => FALSE,
+//    'cachedir' => '',
+//    'char_set' => 'utf8',
+//    'dbcollat' => 'utf8_general_ci',
+//    'swap_pre' => '',
+//    'encrypt' => FALSE,
+//    'compress' => FALSE,
+//    'stricton' => FALSE,
+//    'failover' => array(),
+//    'save_queries' => TRUE
+// );
